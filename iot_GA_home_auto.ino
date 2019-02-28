53a73275b89d30c041eb6fe3e11d0c261036414e
Auroshis Ray <47098039+Auroshis@users.noreply.github.com>
2019-02-28 10:34:41 +0530

    Add files via upload

---
+++ iot_GA_home_auto.ino
@@ -0,0 +1,143 @@
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+#define relay1            D6
+#define relay2            D5
+#define relay3            D2
+#define relay4            D1
+
+#define WLAN_SSID       "Ray Corp"             // Your SSID CHANGE AS PER YOUR REQ
+#define WLAN_PASS       "madhupatna"        // Your password CHANGE AS PER YOUR REQ
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME    "rayauroshis"            // Replace it with your username OF ADAFRUIT ID
+#define AIO_KEY         "1c968a80212e4170a97b24358bb5b55b"   // Replace with your Project Auth Key INT ADAFRUIT ID DASH BOARD
+
+/************ Global State (you don't need to change this!) ******************/
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+// or... use WiFiFlientSecure for SSL
+//WiFiClientSecure client;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+/****************************** Feeds ***************************************/
+
+
+// Setup a feed called 'onoff' for subscribing to changes.
+Adafruit_MQTT_Subscribe Light1 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME"/feeds/r1"); // FeedName
+Adafruit_MQTT_Subscribe Light2 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/relay2");
+Adafruit_MQTT_Subscribe Light3 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/relay3");
+Adafruit_MQTT_Subscribe Light4 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/relay4");
+
+
+
+void MQTT_connect();
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(relay1, OUTPUT);
+  pinMode(relay2, OUTPUT);
+  pinMode(relay3, OUTPUT);
+  pinMode(relay4, OUTPUT);
+  
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); 
+  Serial.println(WiFi.localIP());
+ 
+
+  // Setup MQTT subscription for onoff feed.
+  mqtt.subscribe(&Light1);
+  mqtt.subscribe(&Light3);
+  mqtt.subscribe(&Light2);
+  mqtt.subscribe(&Light4);
+}
+
+void loop() {
+ 
+  MQTT_connect();
+  
+
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(20000))) {
+    if (subscription == &Light1) {
+      Serial.print(F("Got: "));
+      Serial.println((char *)Light1.lastread);
+      int Light1_State = atoi((char *)Light1.lastread);
+      digitalWrite(relay1, Light1_State);
+      
+    }
+    if (subscription == &Light2) {
+      Serial.print(F("Got: "));
+      Serial.println((char *)Light2.lastread);
+      int Light2_State = atoi((char *)Light2.lastread);
+      digitalWrite(relay2, Light2_State);
+    }
+    if (subscription == &Light3) {
+      Serial.print(F("Got: "));
+      Serial.println((char *)Light3.lastread);
+      int Light3_State = atoi((char *)Light3.lastread);
+      digitalWrite(relay3, Light3_State);
+    }
+    if (subscription == &Light4) {
+      Serial.print(F("Got: "));
+      Serial.println((char *)Light4.lastread);
+      int Light4_State = atoi((char *)Light4.lastread);
+      digitalWrite(relay4, Light4_State);
+      
+    }
+  }
+
+  
+}
+
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000);  // wait 5 seconds
+    retries--;
+    if (retries == 0) {
+      // basically die and wait for WDT to reset me
+      while (1);
+    }
+  }
+  Serial.println("MQTT Connected!");
+  
+} 
+NOTE:YOU ONLY HAVRE TO CHANGE YOUR SSID ,PASSWORD,PROJECT AUTH KEY,USERNAME AS PER THE REQUIREMENT AND THE NAME OF THE FEED ,EXACTLY AS YOU HAVE WRITTEN IN THE ADAFRUIT ID PROJECT DASHBOARD.
+THE PHRASES I USED WERE “ACTION “ OR “IT’S TOO DARK” TO SWITCH ON WHILE 
+“LIGHT OFF” TO TURN IT OFF.
+
+
